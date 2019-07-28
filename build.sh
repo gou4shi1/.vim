61bb836c2f0aa037c87c4ceba67c94ad594845bb
guangqing.chen <guangqing.chen@weride.ai>
2019-07-28 18:18:03 +0800

    [fix] add official ycm back and delete colored_code from build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@ echo 'YouCompleteMe'
 cd ~/.vim/pack/completion/start/YouCompleteMe
 python3 install.py --clang-completer --ts-completer
 
-echo 'color_coded'
-cd ~/.vim/pack/highlight/start/color_coded
-./build.sh
-
 cp ~/.vim/.style.yapf ~/.style.yapf
