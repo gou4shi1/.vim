6f9583d15fcce710d2c72f820bcc1e739d3c222b
guangqing.chen <guangqing.chen@weride.ai>
2020-09-27 17:23:19 +0800

    [install.sh] auto download vscode-pylance

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ pack generate
 #python3 install.py --clangd-completer --ts-completer
 
 cp ~/.vim/.style.yapf ~/.style.yapf
+
+# Download vscode-pylance
+mkdir /tmp/download-vscode-pylance
+cd /tmp/download-vscode-pylance
+wget https://ms-python.gallery.vsassets.io/_apis/public/gallery/publisher/ms-python/extension/vscode-pylance/2020.9.6/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+unzip Microsoft.VisualStudio.Services.VSIXPackage
+rm -rf ~/.config/coc/extensions/vscode-pylance
+mv extension ~/.config/coc/extensions/vscode-pylance
