0b3ac26a92e8de0762bc1ce2c82ca77945788a4c
guangqing.chen <guangqing.chen@weride.ai>
2022-09-13 23:44:44 +0800

    [scripts] check python3 for LeaderF

--- scripts/pack-install.sh
+++ scripts/pack-install.sh
@@ -4,6 +4,11 @@ set -e
 
 source ~/.vim/scripts/utils.sh
 
+if ! command -v python3 &> /dev/null; then
+    log::fail "python3 command not found!"
+    exit 1
+fi
+
 log::info "Clear ~/.vim/pack (backup as ~/.vim/pack.bak)."
 mkdir -p ~/.vim/pack
 rm -rf ~/.vim/pack.bak
